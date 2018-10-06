84eca6aeaf93a9d0250f40949d9b0b65feb32840
facundo-lobo <facundolobo_42@hotmail.com>
2018-10-06 00:28:05 -0300

    agregacion de atributos y modificacion
    nuevos atributos para contrato , modificacion de atributos de propiedad

--- grails-app/domain/inmobiliaria/Propiedad.groovy
+++ grails-app/domain/inmobiliaria/Propiedad.groovy
@@ -2,11 +2,12 @@ package inmobiliaria
 
 class Propiedad {
 	//Cliente cliente;
+	int id_propietario;//cliente dueño de la propiedad
 	String ubicacion;
 	String descripcion;
-	String titular;
 	int id_propiedad;
-	String tipo_propiedad // debe tener un tipo diferente para "casas ,departamentos , locales , etc"
+	String tipo_propiedad; // debe tener un tipo diferente para "casas ,departamentos , locales , etc"
+	String estado_propiedad; // si esta alquilada o vendida , o libre 
 
     static constraints = {
     }
