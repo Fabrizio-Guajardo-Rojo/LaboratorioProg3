c2bdd05038436571d421063496ebfa86892c26a7
facundo-lobo <facundolobo_42@hotmail.com>
2018-10-06 00:08:52 -0300

    agregacion de atributos
    primeros atributos de la clase propiedad

--- grails-app/domain/inmobiliaria/Propiedad.groovy
+++ grails-app/domain/inmobiliaria/Propiedad.groovy
@@ -1,6 +1,12 @@
 package inmobiliaria
 
 class Propiedad {
+	//Cliente cliente;
+	String ubicacion;
+	String descripcion;
+	String titular;
+	int id_propiedad;
+	String tipo_propiedad // debe tener un tipo diferente para "casas ,departamentos , locales , etc"
 
     static constraints = {
     }
