b149e5655dc6befd14163bbd73bf852736e1211e
facundo-lobo <facundolobo_42@hotmail.com>
2018-10-12 18:47:09 -0300

    arreglo de atributos de clases

--- grails-app/domain/inmobiliaria/Propiedad.groovy
+++ grails-app/domain/inmobiliaria/Propiedad.groovy
@@ -2,10 +2,9 @@ package inmobiliaria
 
 class Propiedad {
 	//Cliente cliente;
-	int id_propietario;//cliente dueño de la propiedad
+	//int id_propietario;//cliente dueño de la propiedad
 	String ubicacion;
 	String descripcion;
-	int id_propiedad;
 	String tipo_propiedad; // debe tener un tipo diferente para "casas ,departamentos , locales , etc"
 	String estado_propiedad; // si esta alquilada o vendida , o libre 
 
