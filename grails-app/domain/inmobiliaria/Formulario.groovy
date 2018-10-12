73cb66fa835fc75dde66a1407a9dcb3beb5436a2
DanielAndreatta <danielandreatta2@gmail.com>
2018-10-12 20:01:10 -0300

    agregue

---
+++ grails-app/domain/inmobiliaria/Formulario.groovy
@@ -0,0 +1,12 @@
+package inmobiliaria
+
+class Formulario {
+    nombre_formulario
+    apellido_formulario
+    telefono_formulario
+    email_formulario
+    detalle_formulario
+
+    static constraints = {
+    }
+}
--- grails-app/domain/inmobiliaria/Usuario.groovy
+++ grails-app/domain/inmobiliaria/Usuario.groovy
@@ -1,6 +1,12 @@
 package inmobiliaria
 
 class Usuario {
+    String nombre_usuario
+    nombre
+    apellido
+    String password
+    String tipo_usuario
+    String email
 
     static constraints = {
     }
