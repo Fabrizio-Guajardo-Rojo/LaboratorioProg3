969722cc76e08ecb8b72d98a048edcee9e6b22fe
DanielAndreatta <danielandreatta2@gmail.com>
2018-10-19 16:31:27 -0300

    agregue validaciones

--- grails-app/domain/inmobiliaria/Formulario.groovy
+++ grails-app/domain/inmobiliaria/Formulario.groovy
@@ -1,12 +1,17 @@
 package inmobiliaria
 
 class Formulario {
-    nombre_formulario
-    apellido_formulario
-    telefono_formulario
-    email_formulario
-    detalle_formulario
+    String nombre_formulario
+    String apellido_formulario
+    Integer telefono_formulario
+    String email_formulario
+    String detalle_formulario
 
     static constraints = {
+      nombre_formulario (blank:false, maxSize:50)
+      apellido_formulario (blank:false, maxSize:50)
+      telefono_formulario (blank:false)
+      email_formulario (blank:false , email:true)
+      detalle_formulario (blank:false, maxSize: 300)
     }
 }
--- grails-app/domain/inmobiliaria/Usuario.groovy
+++ grails-app/domain/inmobiliaria/Usuario.groovy
@@ -1,13 +1,19 @@
 package inmobiliaria
 
 class Usuario {
-    String nombre_usuario
-    nombre
-    apellido
+    String login
+    String nombre
+    String apellido
     String password
     String tipo_usuario
     String email
 
     static constraints = {
+      login (login size: 5..15, blank: false, unique: true)
+      nombre (blank:false, maxSize:50)
+      apellido (blank:false, maxSize:20)
+      tipo_usuario(blank:false, inList:['administrador','operador'])
+      password (size: 5..15, blank: false)
+      email (email: true, blank: false)
     }
 }
