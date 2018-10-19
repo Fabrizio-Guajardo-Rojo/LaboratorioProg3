3a2d28ed325c60ea83d40610ebc55d47afd2ab8b
facundo-lobo <facundolobo_42@hotmail.com>
2018-10-19 19:43:00 -0300

    nuevas clases

---
+++ grails-app/domain/inmobiliaria/Propiedad.groovy
@@ -0,0 +1,14 @@
+package inmobiliaria
+
+class Propiedad {
+		//Cliente cliente;
+	//int id_propietario;//cliente dueño de la propiedad
+	String ubicacion;
+	String descripcion;
+	String tipo_propiedad; // debe tener un tipo diferente para "casas ,departamentos , locales , etc"
+	String estado_propiedad; // si esta alquilada o vendida , o libre 
+
+
+    static constraints = {
+    }
+}
