1e8000fcef4d87ca60142623bb82fe70374bfccc
DanielAndreatta <danielandreatta2@gmail.com>
2018-10-22 18:21:06 -0300

    correji error

--- grails-app/domain/inmobiliaria/Cliente.groovy
+++ grails-app/domain/inmobiliaria/Cliente.groovy
@@ -19,8 +19,8 @@ class Cliente {
         apellido_cliente (blank: false, maxSize:20)
         dni_cliente (blank: false, unique: true)
         domicilio_cliente (blank: false, maxSize: 100)
-        telefono_cliente (blank: false, [0-9]{3}[-][0-9]{6})
+        telefono_cliente (blank: false, matches: "[0-9]{3}[-][0-9]{6}")
         email_cliente (blank: false, email: true)
-        tipo_cliente = [] (blank: false, inList:['Comprador','Vendedor'])
+        tipo_cliente (blank: false, inList:['Comprador','Vendedor'])
     }
 }
