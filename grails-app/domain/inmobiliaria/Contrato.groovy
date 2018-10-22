c5ab77afe636cbbf68a7c6a743f7ed0623c76ff6
facundo-lobo <facundolobo_42@hotmail.com>
2018-10-21 21:41:12 -0300

    reparacion de clases
    nuevas restricciones y enlases con otras clases

--- grails-app/domain/inmobiliaria/Contrato.groovy
+++ grails-app/domain/inmobiliaria/Contrato.groovy
@@ -7,7 +7,9 @@ class Contrato {
 	String descripcion;
 	float importe;
 	Date fecha;
-
+//static belongsTo = [cliente_dueño: Cliente] // el dueñño de la propiedad
+//static belongsTo = [cliente_comprador: Cliente] // el cliente de la propiedad
+static belongsTo = [propiedad: Propiedad] // la propiedad en cuestion
 
     static constraints = {
     	descripcion (blank:false , maxSize:50)
--- grails-app/domain/inmobiliaria/Propiedad.groovy
+++ grails-app/domain/inmobiliaria/Propiedad.groovy
@@ -1,18 +1,22 @@
 package inmobiliaria
 
 class Propiedad {
-		//Cliente cliente;
+	//Cliente cliente;
+	 // solo tiene un dueño
 	//int id_propietario;//cliente dueño de la propiedad
 	String ubicacion;
 	String descripcion;
 	String tipo_propiedad; // debe tener un tipo diferente para "casas (c),departamentos(d) , locales (l), etc"
 	String estado_propiedad; // si esta alquilada o vendida , o libre 
+	//static belongsTo = [cliente_dueño: Cliente]  // falta agregar el "static hasMany = [propiedades: Propiedad]"
 
 
-    static constraints = {}
+    static constraints = {
     
     descripcion (maxSize:500)
-    
+    ubicacion (maxSize:500)
+    tipo_propiedad (maxSize:500)
+    estado_propiedad (maxSize:500)
    
 
     }
