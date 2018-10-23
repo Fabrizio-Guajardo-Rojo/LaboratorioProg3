bb0cc49fb5f6bd07a1fa52c3713d86d270870de5
DanielAndreatta <danielandreatta2@gmail.com>
2018-10-23 01:15:00 -0300

    agregue un atributo

--- grails-app/domain/inmobiliaria/Formulario.groovy
+++ grails-app/domain/inmobiliaria/Formulario.groovy
@@ -4,6 +4,7 @@ class Formulario {
     String nombre_formulario
     String apellido_formulario
     Integer telefono_formulario
+    String domicilio_formulario
     String email_formulario
     String detalle_formulario
 
@@ -12,6 +13,7 @@ class Formulario {
       apellido_formulario (blank:false, maxSize:50)
       telefono_formulario (blank:false)
       email_formulario (blank:false , email:true)
+      domicilio_formulario (maxSize: 100)
       detalle_formulario (blank:false, maxSize: 300)
     }
 }
